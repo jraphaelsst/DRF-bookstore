45ef4525d14d134089a126e09cf9b428f532c4a8
jraphaelsst <joaoraphaelsst@gmail.com>
2023-12-15 16:54:48 +1100

    setup docker-compose & postgres image

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -63,6 +63,19 @@ DATABASES = {
     }
 }
 
+'''
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
+'''
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
